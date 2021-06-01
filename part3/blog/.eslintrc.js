3a926a42cb3765e2cf819bf3b94ea0e1c220291c
kahijes <j.kahilainen@live.fi>
2021-06-01 22:20:14 +0300

    5d

--- part3/blog/.eslintrc.js
+++ part3/blog/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     'commonjs': true,
     'es2021': true,
     'node': true,
-    'jest': true
+    'jest': true,
+    
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
--- part3/blog/app.js
+++ part3/blog/app.js
@@ -28,6 +28,13 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testingRouter')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
