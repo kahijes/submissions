933f0a9e5b5174254130d9619f20c2ff4838e1a4
kahijes <j.kahilainen@live.fi>
2021-06-02 17:12:01 +0300

    6a

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdotesList from './components/AnecdotesList'
+
+const App = () => {  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdotesList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/components/AnecdotesList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote }  from '../reducers/anecdoteReducer'
+
+const AnecdotesList = (props) => {
+  
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  anecdotes.sort(function (a, b) {
+    return b.votes - a.votes
+  })
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteAnecdote(id))
+  }
+
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+    )}
+    </div>
+  )
+}
+export default AnecdotesList
